81ec8feee448a41f40817c50613d5fbab39b5673
SheedyWonder <rasheed.saka85@yahoo.com>
2020-09-22 18:48:19 -0400

    Complete

--- src/main/java/cd/get/ready/algorithms/WuTangForeverBuzz.java
+++ src/main/java/cd/get/ready/algorithms/WuTangForeverBuzz.java
@@ -1,8 +1,23 @@
 package cd.get.ready.algorithms;
-
+import java.util.*;
+import java.io.*;
 public class WuTangForeverBuzz {
 
-    public String wuTangClan(int value){
-        return null;
+    public String wuTangClan(int value)
+    {
+        for(int i = 0; i < value; i++ )
+        {
+            if(value % 3 == 0)
+            return "Wu";
+
+            if(value % 5 == 0)
+            return "Tang";
+
+            if(value % 3 == 0 && value % 5 == 0)
+             return "WuTang Forever";
+
+            else{return String.valueOf(i+1) + String.valueOf(value);}
+        }
+        return String.valueOf(value);
     }
 }
