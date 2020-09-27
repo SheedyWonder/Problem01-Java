1fb024a918457c22bcc35cae07089eade945ca5c
SheedyWonder <rasheed.saka85@yahoo.com>
2020-09-27 03:35:35 -0400

    Complete

--- src/main/java/cd/get/ready/algorithms/WuTangForeverBuzz.java
+++ src/main/java/cd/get/ready/algorithms/WuTangForeverBuzz.java
@@ -8,13 +8,13 @@ public class WuTangForeverBuzz {
         for(int i = 0; i < value; i++ )
         {
             if(value % 3 == 0)
-            return "Wu";
+            return "Wu" + String.valueOf(value);
 
             if(value % 5 == 0)
-            return "Tang";
+            return "Tang" + String.valueOf(value);
 
             if(value % 3 == 0 && value % 5 == 0)
-             return "WuTang Forever";
+             return "WuTang Forever" + String.valueOf(value);
 
             else{return String.valueOf(i+1) + String.valueOf(value);}
         }
